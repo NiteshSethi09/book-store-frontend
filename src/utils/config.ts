ef9dd34892e024445688156c343da7e9b26b559b
Nitesh Sethi <nonusethi1272@gmail.com>
2023-04-02 18:06:50 +0530

    Changed prod URL

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const endpoints = {
   localhost: "http://localhost:5000",
-  prod: "https://book-store-backend-six.vercel.app",
+  prod: "https://book-store-backend.onrender.com",
 };
 
 export const razorpayKeyId = import.meta.env.VITE_RAZORPAY_KEY_ID;
